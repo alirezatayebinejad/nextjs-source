0017548169c2d02aeaacd382d82855084b4f1d56
AlirezaTN <alirezatayebinejad@gmail.com>
2024-01-27 16:55:29 +0330

    redirect

--- src/app/tutorials/2.routing/1.specialFiles.tsx
+++ src/app/tutorials/2.routing/1.specialFiles.tsx
@@ -77,3 +77,33 @@ export function Posts() {
 		</section>
 	);
 }
+
+/* Error handling
+
+    An error.js boundary will not handle errors thrown in a layout.js component in the same segment because the error boundary is nested inside that layout's component.
+    To handle errors within the root layout or template, use a variation of error.js called global-error.js in app/
+*/
+
+("use client"); // Error components must be Client Components
+import { useEffect } from "react";
+
+export function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+	useEffect(() => {
+		// Log the error to an error reporting service
+		console.error(error);
+	}, [error]);
+
+	return (
+		<div>
+			<h2>Something went wrong!</h2>
+			<button
+				onClick={
+					// Attempt to recover by trying to re-render the segment
+					() => reset()
+				}
+			>
+				Try again
+			</button>
+		</div>
+	);
+}
--- src/app/tutorials/2.routing/2.navigation.tsx
+++ src/app/tutorials/2.routing/2.navigation.tsx
@@ -26,6 +26,41 @@ export function Page() {
 	);
 }
 
+//redirects
+/*
+redirect  ====> 	Redirect user after a mutation or event - in Server Components, Server Actions, Route Handlers	307 (Temporary) or 303 (Server Action)
+permanentRedirect ====>	Redirect user after a mutation or event	Server in Components, Server Actions, Route Handlers	308 (Permanent)
+useRouter ====>	Perform a client-side navigation Event Handlers in Client Components	N/A
+redirects in next.config.js ====>	Redirect an incoming request based on a path - in next.config.js file	307 (Temporary) or 308 (Permanent)
+NextResponse.redirect ====>	Redirect an incoming request based on a condition - inMiddleware	Any
+
+*/
+
+/* redirect and permanentRedirect 
+307 (Temporary Redirect) status code by default
+in a Server Action, it returns a 303 (See Other) commonly used for redirecting to a success page as a result of a POST request.
+redirect can be called in Client Components during the rendering process but not in event handlers
+permanentRedirect returns a 308 (permanent redirect) status code by default
+If you'd like to redirect before the render process, use next.config.js or Middleware.
+
+*/
+("use server");
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+
+export async function createPost(id: string) {
+	try {
+		// Call database
+	} catch (error) {
+		// Handle errors
+	}
+
+	revalidatePath("/posts"); // Update cached posts
+	redirect(`/post/${id}`); // Navigate to the new post page
+	//or
+	//permanentRedirect(`/profile/${username}`)
+}
+
 /* useRouter hook
    programmatically change routes from Client Components
 
@@ -70,3 +105,51 @@ export function NavigationEvents() {
 
 	return null;
 }
+
+/* redirects in next.config.js
+
+	redirects may have a limit on platforms. For example, on Vercel, there's a limit of 1,024 redirects. To manage a large number of redirects (1000+), consider creating a custom solution using Middleware.
+	redirects runs before Middleware.
+
+*/
+module.exports = {
+	async redirects() {
+		return [
+			// Basic redirect
+			{
+				source: "/about",
+				destination: "/",
+				permanent: true,
+			},
+			// Wildcard path matching
+			{
+				source: "/blog/:slug",
+				destination: "/news/:slug",
+				permanent: true,
+			},
+		];
+	},
+};
+
+/* NextResponse.redirect in Middleware
+  Middleware allows you to run code before a request is completed
+ 
+  */
+import { NextResponse, NextRequest } from "next/server";
+//import { authenticate } from 'auth-provider'
+
+export function middleware(request: NextRequest) {
+	//const isAuthenticated = authenticate(request)
+
+	// If the user is authenticated, continue as normal
+	/* if (isAuthenticated) {
+	  return NextResponse.next()
+	} */
+
+	// Redirect to login page if not authenticated
+	return NextResponse.redirect(new URL("/login", request.url));
+}
+
+export const config = {
+	matcher: "/dashboard/:path*",
+};
